b22b67cae75c86a2328022db0b00609fc17ed6b2
CodingLama <kalbande.pranit@gmail.com>
2019-04-24 15:14:42 +0530

    currency updated paypal

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -25,7 +25,7 @@ export default class MyApp extends React.Component {
         }
  
         let env = 'sandbox'; // you can set here to 'production' for production
-        let currency = 'USD'; // or you can set this value from your props or state
+        let currency = 'INR'; // or you can set this value from your props or state
         // let total = 1; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
         // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
  
